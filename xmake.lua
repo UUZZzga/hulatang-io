0dff911597077ded7ec4dbfa9cf0388076a8c3ea
uzzzzzzzzzzzzzz <zga786338112@outlook.com>
2022-03-01 09:30:29 +0800

    初始化

---
+++ xmake.lua
@@ -0,0 +1,87 @@
+add_rules("mode.debug", "mode.release")
+
+set_languages("c++20")
+
+-- 拉取远程依赖
+add_requires("gtest")
+
+set_warnings("all")
+
+-- 关闭警告
+if is_plat("windows") then
+    add_cxxflags("/wd4819")
+end
+
+-- 配置模式行为
+if is_mode("debug") then
+    -- 添加DEBUG编译宏
+    add_defines("_DEBUG")
+
+    -- 启用调试符号
+    set_symbols("debug")
+
+    -- 禁用优化
+    set_optimize("none")
+
+    if is_plat("windows") then
+        set_runtimes("MTd")
+    end
+else
+    -- 添加NDEBUG编译宏
+    add_defines("NDEBUG")
+
+    -- set_optimize("fastest")    -- O3
+    set_optimize("faster")    -- O2
+
+    if is_plat("windows") then
+        set_runtimes("MT")
+    end
+end
+
+-- 启用协程支持
+if is_plat("windows") then
+    add_cxxflags("/await:strict") -- _COROUTINE_ABI = 2
+elseif is_host("macosx") then
+    add_cxxflags("-fcoroutines-ts")
+else
+    add_cxxflags("-fcoroutines","-fconcepts-diagnostics-depth=8")
+end
+
+--系统库
+if is_host("windows") then
+    add_syslinks("Ws2_32","Shell32")
+elseif is_host("linux") then
+    add_syslinks("pthread")
+end
+
+add_includedirs(".")
+
+if is_plat("mingw") then
+    -- add_cflags("-D__MSVCRT_VERSION__=0x1100","-D__USE_MINGW_ACCESS")
+    -- add_cxxflags("-D__MSVCRT_VERSION__=0x1100","-D__USE_MINGW_ACCESS")
+    add_ldflags("-nostdlib")
+    add_syslinks("mingw32","gcc","mingwex","msvcr110")
+end
+
+includes("check_cfuncs.lua") -- configvar_check_cfuncs
+includes("check_cxxsnippets.lua") -- configvar_check_cxxsnippets
+
+target("config")
+    set_kind("headeronly")
+    -- 生成配置文件
+    configvar_check_cfuncs("HAVE_FWRITE_UNLOCKED", 'fwrite_unlocked',{includes = {"stdio.h"}})
+    configvar_check_cfuncs("HAVE__FWRITE_NOLOCK", '_fwrite_nolock',{includes = {"stdio.h"}})
+    configvar_check_cfuncs("HAVE_EPOLL_CREATE1", 'epoll_create1',{includes = {"sys/epoll.h"}})
+    configvar_check_cfuncs("HAVE_EPOLL_CREATE", 'epoll_create',{includes = {"sys/epoll.h"}})
+
+    add_configfiles("hulatang/base/config.h.in")
+
+    after_build(function (target)
+        -- 默认生成目录为 $(buildir)/config.h
+        target:add("headerfiles", "$(buildir)/config.h")
+    end)
+
+target("config")
+    set_kind("shared")
+
+    add_files("hulatang/io/*.cpp")
